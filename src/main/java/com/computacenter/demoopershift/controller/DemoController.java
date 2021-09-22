342e64a3229b1a4adc7d965e711f055923450ea2
gsshri1 <67909865+gsshri1@users.noreply.github.com>
2021-09-22 12:02:37 +0200

    Api vereinfacht

--- src/main/java/com/computacenter/demoopershift/controller/DemoController.java
+++ src/main/java/com/computacenter/demoopershift/controller/DemoController.java
@@ -5,10 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "api/demo")
 public class DemoController {
 
-    @GetMapping(value = "/home")
+    @GetMapping(value = "/")
     public String Home(){
         return "Welcome to the demo of openshift";
     }
